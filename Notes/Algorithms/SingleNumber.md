d11140c14663aff2d0c4183336237574bb905b44
Holt Walborn <h.walborn@criteo.com>
2021-09-10 21:39:53 -0400

    Single Number

---
+++ Notes/Algorithms/SingleNumber.md
@@ -0,0 +1,6 @@
+* Hmmmm.. Lets make a dictionary of int, int?
+* loop over array
+    * check if dictionary has key
+        * if not? -> add key
+        * if so? remove key/value
+* at the end, the dictionary should have one key -> the non reocurring number;
--- Problems/Algorithms/SingleNumber.cs
+++ Problems/Algorithms/SingleNumber.cs
@@ -1,12 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problems.Algorithms
 {
     public class SingleNumber : ITest
     {
+        private int Solution(int[] nums)
+        {
+            var resultsDictionary = new Dictionary<int, int?>();
+            foreach (var num in nums)
+            {
+                if (resultsDictionary.ContainsKey(num))
+                {
+                    resultsDictionary.Remove(num);
+                }
+                else
+                {
+                    resultsDictionary.Add(num, null);
+                }
+            }
+            return resultsDictionary.Keys.First();
+        }
         public void Go()
         {
-            Console.WriteLine("YOU GOTTA IMPLEMENT THE GO METHOD, HOLT!");
+            Console.WriteLine(Solution(new int[] { 2,2,1 }));
+            Console.WriteLine(Solution(new int[] { 1 }));
+            Console.WriteLine(Solution(new int[] { 4,1,2,1,2 }));
         }
     }
 }
